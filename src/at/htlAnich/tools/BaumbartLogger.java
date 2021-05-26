9a74776c69486b4de0049b8d3b0b03619c4ecbd0
Baumbart13 <thomaskefer0109@gmail.com>
2021-05-26 20:09:26 +0200

    Added logging-methods.

--- src/at/htlAnich/tools/BaumbartLogger.java
+++ src/at/htlAnich/tools/BaumbartLogger.java
@@ -4,8 +4,14 @@ public final class BaumbartLogger {
 	public static void logf(String s, Object ... o){
 		System.out.printf(s, o);
 	}
+	public static void loglnf(String s, Object ... o){
+		logf(s.concat("%n"), o);
+	}
 	public static void errf(String s, Object ... o){
 		System.err.printf(s, o);
+	}
+	public static void errlnf(String s, Object ... o){
+		errf(s.concat("%n"), o);
 	}
 	public static void waitForKeyPress(){var s = new java.util.Scanner(System.in); s.nextLine();}
 }
